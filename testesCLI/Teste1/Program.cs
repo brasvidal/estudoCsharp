baf725658df8d533cf1833e1e1de5a459f1e3cff
Marcos Vidal <marcosvidal@outlook.com>
2021-11-27 10:22:27 -0300

    Added Calculator

--- testesCLI/Teste1/Program.cs
+++ testesCLI/Teste1/Program.cs
@@ -7,6 +7,18 @@ namespace Teste1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Calcula(2, 3);
+        }
+
+        private static void Calcula(int v1, int v2)
+        {
+            var total = 0;
+            total = v1 + v2;
+            Console.WriteLine(total);
+
+
+
         }
     }
 }
