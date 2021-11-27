a7ac5b929e96c13d8c7e5aecc5b8300d34a69248
Marcos Vidal <marcosvidal@outlook.com>
2021-11-27 11:09:51 -0300

    Stopwatch

--- testesCLI/Calculator/Program.cs
+++ testesCLI/Calculator/Program.cs
@@ -6,7 +6,22 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double v1;
+            double v2;
+            Console.Clear();
+            v1 = PegaValores();
+
+
+            Soma();
         }
+
+        static double PegaValores(double valor)
+        {
+            Console.WriteLine("Insira o valor: ");
+            valor = Convert.ToDouble(Console.ReadLine());
+            return valor;
+        }
+
     }
 }
+//double v1 = Convert.ToDouble(Console.ReadLine());
